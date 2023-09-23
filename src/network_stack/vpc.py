d2447bae6a372a5b7af64229e126fec160561a14
Muhammad Fahad Mustafa <mfahadm8@gmail.com>
2023-09-23 14:39:07 +0500

    added vpc creation logic

--- src/network_stack/vpc.py
+++ src/network_stack/vpc.py
@@ -12,44 +12,108 @@ class Vpc(Construct):
     def __init__(self, scope: Construct, id: str, config: Dict) -> None:
         super().__init__(scope, id)
         self.config = config
-
-        self.__build_subnets_config()
         self.__create_vpc()
 
     def __create_vpc(self):
+        # Configuration
         vpc_config = self.config["network"]["vpc"]
-        if self.config["network"]["vpc"]["create_natgateway"] == 1:
-            self.vpc = ec2.Vpc(
-                scope=self,
-                id=self.config["name"],
-                nat_gateways=1,
-                subnet_configuration=self.subnet_configuration,
-                max_azs=vpc_config["maxAzs"],
-                cidr=vpc_config["cidr"],
-                nat_gateway_subnets=ec2.SubnetSelection(
-                    subnet_group_name=vpc_config["natGatewaySubnetName"]
-                ),
-                enable_dns_hostnames=True,
-                enable_dns_support=True,
-            )
-        else:
-            self.vpc = ec2.Vpc(
-                scope=self,
-                id=self.config["name"],
-                nat_gateways=0,
-                subnet_configuration=self.subnet_configuration,
-                max_azs=vpc_config["maxAzs"],
-                cidr=vpc_config["cidr"],
-                enable_dns_hostnames=True,
-                enable_dns_support=True,
+        private_subnets_config = self.config["network"]["subnets"]["private"]
+        public_subnets_config = self.config["network"]["subnets"]["public"]
+
+        # Create VPC
+        vpc = ec2.Vpc(
+            self,
+            "app-sp16" + self.config["stage"],
+            cidr=vpc_config["cidr"],
+            enable_dns_support=True,
+            enable_dns_hostnames=True,
+            max_azs=vpc_config["maxAzs"],
+        )
+
+        # Create Internet Gateway
+        internet_gateway = ec2.CfnInternetGateway(
+            self,
+            "InternetGateway",
+        )
+
+        # Attach Internet Gateway to VPC
+        ec2.CfnVPCGatewayAttachment(
+            self,
+            "GatewayToInternet",
+            vpc_id=vpc.vpc_id,
+            internet_gateway_id=internet_gateway.ref,
+        )
+
+        # Create one Private and one Public Route Table
+        private_route_table = ec2.CfnRouteTable(
+            self,
+            "PrivateRouteTable",
+            vpc_id=vpc.vpc_id,
+        )
+
+        public_route_table = ec2.CfnRouteTable(
+            self,
+            "PublicRouteTable",
+            vpc_id=vpc.vpc_id,
+        )
+
+        nat_gateway_subnet = None
+        nat_gateway_eip = ec2.CfnEIP(self, "NATGatewayEIP")
+
+        # Iterate over public subnets configuration
+        for idx, subnet_config in enumerate(public_subnets_config):
+            public_subnet = ec2.Subnet(
+                self,
+                f"PublicSubnet{idx}",
+                vpc=vpc,
+                availability_zone=subnet_config["avl_zone"],
+                cidr_block=subnet_config["cidr_block"],
             )
 
-    def __build_subnets_config(self):
-        for subnet in self.config["network"]["subnets"]:
-            self.subnet_configuration.append(
-                ec2.SubnetConfiguration(
-                    name=subnet["name"],
-                    subnet_type=ec2.SubnetType[subnet["subnetType"]],
-                    cidr_mask=subnet["cidrMask"],
-                )
+            # Associate Public Subnets with the Public Route Table
+            ec2.CfnSubnetRouteTableAssociation(
+                self,
+                f"PublicSubnet{idx}RouteTableAssociation",
+                subnet_id=public_subnet.subnet_id,
+                route_table_id=public_route_table.ref,
+            )
+
+            if idx == 0:
+                # Allocate NAT Gateway EIP and create NAT Gateway in the first public subnet
+                nat_gateway_eip = ec2.CfnEIP(self, "NATGatewayEIP")
+                nat_gateway_subnet = public_subnet
+
+        # Create NAT Gateway in the first public subnet with the allocated EIP
+        nat_gateway = ec2.CfnNatGateway(
+            self,
+            "NATGateway",
+            subnet_id=nat_gateway_subnet.subnet_id,
+            allocation_id=nat_gateway_eip.attr_allocation_id,
+        )
+
+        # Iterate over private subnets configuration
+        for idx, subnet_config in enumerate(private_subnets_config):
+            private_subnet = ec2.Subnet(
+                self,
+                f"PrivateSubnet{idx}",
+                vpc=vpc,
+                availability_zone=subnet_config["avl_zone"],
+                cidr_block=subnet_config["cidr_block"],
+            )
+
+            # Associate Private Subnets with the Private Route Table
+            ec2.CfnSubnetRouteTableAssociation(
+                self,
+                f"PrivateSubnet{idx}RouteTableAssociation",
+                subnet_id=private_subnet.subnet_id,
+                route_table_id=private_route_table.ref,
+            )
+
+            # Add default route to NAT Gateway in the Private Route Table
+            ec2.CfnRoute(
+                self,
+                f"PrivateRoute{idx}",
+                route_table_id=private_route_table.ref,
+                destination_cidr_block="0.0.0.0/0",
+                nat_gateway_id=nat_gateway.ref,
             )
